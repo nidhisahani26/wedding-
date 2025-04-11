adffa13aa091e42c65bc3f41bece0992f462703a
nidhisahani26 <nidhisahani1126@gmail.com>
2025-04-11 11:23:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-document.addEventListener("click", function playAudio() {
+window.addEventListener("DOMContentLoaded", () => {
   const audio = document.getElementById("bg-music");
-  audio.play();
-  document.removeEventListener("click", playAudio);
-});
 
+  // autoplay will work only after user interacts anywhere on the page
+  const enableAutoplay = () => {
+    audio.play();
+    document.removeEventListener("click", enableAutoplay);
+  };
+
+  document.addEventListener("click", enableAutoplay);
+});
 
 document.querySelector('.map-container iframe').addEventListener('click', function() {
   window.open('https://www.google.com/maps?q=St+Peter\'s+Church,+New+York', '_blank');
