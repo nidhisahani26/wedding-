e98887ff36d0d7dbcb15670b5771d9e7b39c59f7
nidhisahani26 <nidhisahani1126@gmail.com>
2025-04-11 12:05:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-window.addEventListener("click", () => {
-    const audio = document.querySelector("audio");
-    audio.muted = false;
-    audio.play();
-  });
+const overlay = document.getElementById("overlay");
+const btn = document.getElementById("enterBtn");
+const music = document.getElementById("bgMusic");
 
+btn.addEventListener("click", () => {
+  overlay.style.display = "none";
+  music.play();
+});
 
 document.querySelector('.map-container iframe').addEventListener('click', function() {
   window.open('https://www.google.com/maps?q=St+Peter\'s+Church,+New+York', '_blank');
